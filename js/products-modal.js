ab70f27545075ebe16060dd700a68b6ccda94b6b
Sokolova-LV <leyla-@ukr.net>
2023-08-28 09:46:20 +0300

    changed 28.08

--- js/products-modal.js
+++ js/products-modal.js
@@ -10,3 +10,29 @@
         modal.classList.toggle("is-hidden");
     }
 })();
+
+(() => {
+    const openModalBtn = document.querySelector("[products2-modal-open]");
+    const closeModalBtn = document.querySelector("[products2-modal-close]");
+    const modal = document.querySelector("[products2-modal]");
+
+    openModalBtn.addEventListener("click", toggleModal);
+    closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        modal.classList.toggle("is-hidden");
+    }
+})();
+
+(() => {
+    const openModalBtn = document.querySelector("[products3-modal-open]");
+    const closeModalBtn = document.querySelector("[products3-modal-close]");
+    const modal = document.querySelector("[products3-modal]");
+
+    openModalBtn.addEventListener("click", toggleModal);
+    closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        modal.classList.toggle("is-hidden");
+    }
+})();
